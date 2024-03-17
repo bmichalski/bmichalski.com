c7732c63bac7fba5a6fc4427693e356bfde58da0
Benjamin Michalski <benjamin.michalski@gmail.com>
2024-03-17 17:42:55 +0100

    Update ability to build and deploy site

--- scripts/paths.ts
+++ scripts/paths.ts
@@ -6,20 +6,22 @@ const root = await fs.realpath(
   `${path.dirname(url.fileURLToPath(import.meta.url))}/../`
 );
 
+const buildDirectory = `${root}/docs`;
+
 export const paths = {
   root,
   index: {
-    src: `${root}/build/about/index.html`,
-    dest: `${root}/build/index.html`,
+    src: `${buildDirectory}/about/index.html`,
+    dest: `${buildDirectory}/index.html`,
   },
   not_found: {
-    src: `${root}/build/page-not-found/index.html`,
-    src_folder: `${root}/build/page-not-found`,
-    dest: `${root}/build/404.html`,
+    src: `${buildDirectory}/page-not-found/index.html`,
+    src_folder: `${buildDirectory}/page-not-found`,
+    dest: `${buildDirectory}/404.html`,
   },
   metalsmith: {
     src: `${root}/src`,
-    dest: `${root}/build`,
+    dest: `${buildDirectory}`,
     layouts: {
       directory: `${root}/layouts`,
       partials: `${root}/partials`,
@@ -29,7 +31,7 @@ export const paths = {
     main: {
       src: ['css/main.css'],
       temp: `${root}/temp/main.min.css`,
-      dest: `${root}/build/css/main.min.css`,
+      dest: `${buildDirectory}/css/main.min.css`,
     },
     vendor: {
       src: [
@@ -39,7 +41,7 @@ export const paths = {
         'node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
       ],
       temp: `${root}/temp/vendor.min.css`,
-      dest: `${root}/build/css/vendor.min.css`,
+      dest: `${buildDirectory}/css/vendor.min.css`,
     },
   },
   js: {
@@ -49,26 +51,26 @@ export const paths = {
         `${root}/node_modules/bootstrap/dist/js/bootstrap.js`,
       ],
       temp: `${root}/temp/vendor.min.js`,
-      dest: `${root}/build/js/vendor.min.js`,
+      dest: `${buildDirectory}/js/vendor.min.js`,
     },
   },
   cname: {
     src: `${root}/src/CNAME`,
-    dest: `${root}/build/CNAME`,
+    dest: `${buildDirectory}/CNAME`,
   },
   img: {
     src: `${root}/img`,
-    dest: `${root}/build/img`,
+    dest: `${buildDirectory}/img`,
   },
   favicon: {
     src: `${root}/src/favicon.ico`,
-    dest: `${root}/build/favicon.ico`,
+    dest: `${buildDirectory}/favicon.ico`,
   },
   fonts: {
     vendor: {
       fontAwesome: {
         srcGlob: `${root}/node_modules/@fortawesome/fontawesome-free/webfonts/*`,
-        destPrefix: `${root}/build/fonts/`,
+        destPrefix: `${buildDirectory}/fonts/`,
       },
     },
   },
